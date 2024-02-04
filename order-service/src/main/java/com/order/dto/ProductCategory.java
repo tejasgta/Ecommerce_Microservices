4a374876f90903ea323c06f260ffc11c309f550f
Tejas Gupta <tejgupta@cisco.com>
2024-02-04 17:41:25 +0530

    Git Config Server

---
+++ order-service/src/main/java/com/order/dto/ProductCategory.java
@@ -0,0 +1,21 @@
+package com.order.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCategory {
+
+	private Long id;
+	private String categoryName;
+	private Set<Product> productSet;
+}
